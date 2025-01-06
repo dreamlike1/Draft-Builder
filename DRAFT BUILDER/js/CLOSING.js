8041bcd5502e97bf54179ff2e9584b1d194b812a
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2025-01-07 07:06:21 +0800

    Delete DRAFT BUILDER directory

--- DRAFT BUILDER/js/CLOSING.js
+++
@@ -1,9 +0,0 @@
-// CLOSING.js
-export const CLOSING_VALUES = [
-    { name: 'Best', value: 'Best' },
-    { name: 'Cheers', value: 'Cheers' },
-    { name: 'Cordially', value: 'Cordially' }
-];
-
-// Debugging statement to check if this file is loaded
-console.log("CLOSING_VALUES loaded:", CLOSING_VALUES);
